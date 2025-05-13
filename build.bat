1c7a679acf45aabdaa696661fcc6e6abad7e8491
NeoMatrix14241 <kylefcapistrano@gmail.com>
2025-05-13 15:58:14 +0800

    update build.bat

--- build.bat
+++ build.bat
@@ -1,5 +1,22 @@
-REM filepath: e:\VisionLane\build.bat
 @echo off
+
+title Build VisionLane Application
+REM --------------------------------------------------------
+REM VisionLane Build Script
+REM --------------------------------------------------------
+
+REM This script is used to build the VisionLane application using PyInstaller.
+REM It cleans up any previous builds and then creates a new build.
+REM Make sure to run this script from the command line with administrative privileges.
+REM This script assumes that you have PyInstaller installed in a virtual environment located at .\.venv\Scripts\pyinstaller
+REM and that the build.spec file is located in the same directory as this script.
+REM This script is intended for use on Windows systems.
+REM --------------------------------------------------------
+REM Filepath:
+REM .\build.bat
+REM .\build.spec
+REM .\.venv\Scripts\pyinstaller
+
 echo Cleaning up old build...
 rmdir /s /q build dist 2>nul
 echo Building application...
