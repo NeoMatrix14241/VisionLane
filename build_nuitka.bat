938bacd5ad46cae44b64c82c0e34b6d18da246ab
NeoMatrix14241 <kylefcapistrano@gmail.com>
2025-05-21 18:31:29 +0800

    update build_nuitka.bat

--- build_nuitka.bat
+++ build_nuitka.bat
@@ -33,12 +33,61 @@ echo Building application with Nuitka...
     --include-data-file=config.ini=config.ini ^
     --include-data-file=README.md=README.md ^
     --include-data-file=LICENSE=LICENSE ^
+    --include-package=doctr ^
+    --include-package=doctr.models ^
+    --include-package=doctr.datasets ^
+    --include-package=doctr.io ^
+    --include-package=doctr.utils ^
+    --include-package=torch ^
+    --include-package=torch.nn ^
+    --include-package=torch.optim ^
+    --include-package=torch.cuda ^
+    --include-package=torchvision ^
+    --include-package=torchvision.transforms ^
+    --include-package=PIL ^
+    --include-package=numpy ^
+    --include-package=cv2 ^
+    --include-package=psutil ^
+    --include-package=pynvml ^
+    --include-package=GPUtil ^
+    --include-package=PyPDF2 ^
+    --include-package=ocrmypdf ^
+    --include-package=ocrmypdf.data ^
+    --include-package=ocrmypdf.api ^
+    --include-package=ocrmypdf.helpers ^
+    --include-package=pdf2image ^
+    --include-package=img2pdf ^
+    --include-package=pdfminer ^
+    --include-package=pdfminer.six ^
+    --include-package=pdfminer.high_level ^
+    --include-package=pdfminer.layout ^
+    --include-package=pdfminer.converter ^
+    --include-package=pdfminer.pdfinterp ^
+    --include-package=pdfminer.pdfpage ^
+    --include-package=PyQt6 ^
+    --include-package=PyQt6.QtWidgets ^
+    --include-package=PyQt6.QtGui ^
+    --include-package=PyQt6.QtCore ^
+    --include-package=PyQt6.QtPrintSupport ^
+    --include-package=multiprocessing ^
+    --include-package=magic ^
+    --include-package=magic.loader ^
+    --include-package=tqdm ^
+    --include-package=colorama ^
+    --include-package=typing_extensions ^
+    --include-package=fitz ^
+    --include-package=pymupdf ^
+    --include-package=gui ^
+    --include-package=utils ^
     --include-package-data=doctr ^
-    --include-package-data=ocrmypdf ^
-    --include-package-data=PyPDF2 ^
-    --include-package-data=pymupdf ^
+    --include-package-data=torch ^
+    --include-package-data=torch.cuda ^
+    --include-package-data=torchvision ^
     --include-package-data=PIL ^
     --include-package-data=pdfminer ^
+    --include-package-data=pymupdf ^
+    --include-package-data=ocrmypdf ^
+    --include-data-dir=.venv\Lib\site-packages\torch\lib=torch\lib ^
     --output-dir=dist ^
     --windows-icon-from-ico=icon.ico ^
     main.py
